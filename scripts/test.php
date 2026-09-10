4c2b2a88f743e8d51853bd5ba291b4330cbd9159
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-09-09 22:26:23 -0400

    Premier test

---
+++ scripts/test.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// echo ini_get('memory_limit').RN;
+// var_dump(Imagick::queryFormats('AVIF'));
+var_dump(IMG::asset('face-de-cul.jpg', 300, 300));
+var_dump(IMG::asset('test/klek.jpg', 300, 300));
+
+// $url = 'https://cihofm.com/news/parc-de-la-cote-de-charlevoix-je-nai-jamais-vu-un-attachement-au-territoire-aussi-frappant/';
+// $url = 'https://cimtchau.ca/nouvelles/parc-de-la-cote-de-charlevoix-un-attachement-frappant-au-territoire/';
+// $url = 'https://www.lecharlevoisien.com/2026/06/02/le-grap-de-baie-sainte-catherine-deposera-un-memoire/';
+// $url = 'https://ici.radio-canada.ca/ohdio/premiere/emissions/le-15-18/segments/rattrapage/2303811/portrait-social-ecosysteme-gigue-au-quebec-meralie-murray-hall';
+// $url = 'https://www.journaldemontreal.com/2026/09/01/le-stress-de-la-pauvrete-endommage-le-cerveau-a-long-terme';
+// $url = 'https://humainhumain.com/services/';
+// print_r(SCRAPER::get($url));
--- src/_index.php
+++ src/_index.php
@@ -2,4 +2,20 @@
 /**
  * @id   home
  * @name Home
+ * content https://raw.githubusercontent.com/php-kirigami/kirigami/refs/heads/main/README.md
  */
+
+// var_dump(IMG::palette('test/klek.jpg'));
+?>
+
+
+<h1>Hello</h1>
+
+<p>Fugiat eu exercitation sit ullamco excepteur nisi amet labore fugiat commodo minim esse amet labore. Cupidatat deserunt anim mollit nisi deserunt Lorem anim velit aliquip enim. Deserunt aliquip velit eiusmod qui voluptate pariatur commodo aliqua commodo excepteur quis. Lorem occaecat laboris voluptate ad irure nisi nisi irure. Consequat tempor officia quis non.</p>
+
+<br>
+<p><img asset="test/klek.jpg" width="450"></p>
+
+<br>
+
+<!-- <img asset="test/klek.jpg" width="600"> -->
